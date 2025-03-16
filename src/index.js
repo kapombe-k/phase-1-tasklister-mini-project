4f2fe15787aedd76504836b5064cc9d696f9fef8
Eugene Kapombe <kapombek@gmail.com>
2025-03-16 22:27:16 +0300

    got to submit task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-document.addEventListener("DOMContentLoaded", function()) {
+document.addEventListener("DOMContentLoaded", function ()) {
   
   const button = document.querySelector('create-new task');
 
-  button.addEventListener('click', function ()){
-  
+  button.addEventListener('click', function ()) {
+    console.log('Task created!')
   }
-}
-const list = [];
+  const list = [];
+  
+  const taskList = document.getElementById('tasks');
 
-const taskList = document.getElementById('tasks');
+  list.forEach(task) 
+    //create a dynamic list using DOM
+    const li = document.createElement('li');
+    li.innerText = task;
+
+  //attach the added item into the list
+  taskList.appendChild(li);
+  };
+    const taskList = [];
+
+    const addTask = document.querySelector('new-task-description');
+    addTask.addEventListener('click', ()) {
+  const listItem = document.getElementById('new-task-description').value;
+
+  taskList.push(listItem);
+  console.log(taskList);
+}
+}
+
