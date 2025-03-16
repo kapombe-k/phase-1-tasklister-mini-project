bc4466e65f9cbbf33b2e5d52a1dc3d9d1c850b18
Eugene Kapombe <kapombek@gmail.com>
2025-03-16 21:14:39 +0300

    started again

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // task list starts here:
-  const listedTask = new tasker();
-
-  /*
-  DOM elements to select:
-  *form, task description, task priority
- */
+document.addEventListener("DOMContentLoaded", function()) {
   
-  const taskForm = document.getElementById("create-task-form"); //form list//
-  const taskDescription = document.getElementById("new-task-description");//description//
-  const taskPriority = document.getElementById("new-task-priority");//priority//
+  const button = document.querySelector('create-new task');
 
-  //tasks list on DOM interaction
-  const ulForTasks = document.getElementById("tasks");
-
-  const addTask = () => (ulForTasks.innerHTML = listedTask.renderTasks());
-
-  //addition of event listeners
-
-  newTaskList.addEventListener("submit", (stopper)) = () => {
-    stopper.preventDefault();
-    tasker.creareNewTask(taskDescription.value);
-    
+  button.addEventListener('click', function ()){
+  
   }
-});
+}
+const list = [];
+
+const taskList = document.getElementById('tasks');
