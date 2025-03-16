1097fb8ab467453d4f3f70955e28d665d416ca7e
Eugene Kapombe <kapombek@gmail.com>
2025-03-16 16:36:09 +0300

    form/input fields

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // task list starts here:
   const listedTask = new listedTask();
 
-  //DOM elements to select:
+  /*
+  DOM elements to select:
+  *form, task description, task priority
+ */
+  
+  const taskForm = document.getElementById("create-task-form"); //form list//
+  const taskDescription = document.getElementById("new-task-description");//description//
+  const taskPriority = document.getElementById("new-task-priority");//priority//
+
+  
 });
