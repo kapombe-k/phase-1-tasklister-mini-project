8e5100abb5e1388a32d9f7b66b621e2a2bbfa858
Eugene Kapombe <kapombek@gmail.com>
2025-03-16 23:56:11 +0300

    after adding if...else statement

--- src/index.js
+++ src/index.js
@@ -10,23 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
   //to get input values
   const taskInput = addTask.value;
 
+  //trying an additional if...else statement to validate text input.
+  if (taskInput !== '') {
+    addTaskToList(taskInput);//function to add task
+    addTask.value = '';
+  } else {
+    alert("Enter task here!")//alert when field is empty
+}
+// const list = []; commented out for now
+
+// Added function keyword and fixed syntax for task list
+function addTaskToList(taskInput){
+  // Create a dynamic list using DOM to add and remove tasks
+  const li = document.createElement("li");
+  li.innerText = taskInput;//list items text
   
-  // const list = []; commented out for now
-
-  // Added function keyword and fixed syntax for task list
-  function addTaskToList(taskInput){
-    // Create a dynamic list using DOM to add and remove tasks
-    const li = document.createElement("li");
-    li.innerText = taskInput;//list items text
-
-    //delete task button
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Remove';
-    deleteButton.addEventListener('click', function () {
-      li.remove();
-    });
-    li.appendChild(deleteButton);//delete button at end of task
-
-    taskList.appendChild(li);
+  //delete task button
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Remove';
+  deleteButton.addEventListener('click', function () {
+    li.remove();
   });
+  li.appendChild(deleteButton);//delete button at end of task
+  
+  taskList.appendChild(li);
+});
 ;
