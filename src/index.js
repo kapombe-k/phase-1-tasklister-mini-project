665432195ef0daf09b8702a2c464c8f58a49637a
Eugene Kapombe <kapombek@gmail.com>
2025-03-17 00:02:14 +0300

    fixed syntax error

--- src/index.js
+++ src/index.js
@@ -16,22 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
     addTask.value = '';
   } else {
     alert("Enter task here!")//alert when field is empty
-}
-// const list = []; commented out for now
+  }
+  // const list = []; commented out for now
 
-// Added function keyword and fixed syntax for task list
-function addTaskToList(taskInput){
-  // Create a dynamic list using DOM to add and remove tasks
-  const li = document.createElement("li");
-  li.innerText = taskInput;//list items text
+  // Added function keyword and fixed syntax for task list
+  function addTaskToList(taskInput) {
+    // Create a dynamic list using DOM to add and remove tasks
+    const li = document.createElement("li");
+    li.innerText = taskInput;//list items text
   
-  //delete task button
-  const deleteButton = document.createElement('button');
-  deleteButton.innerText = 'Remove';
-  deleteButton.addEventListener('click', function () {
-    li.remove();
-  });
-  li.appendChild(deleteButton);//delete button at end of task
+    //delete task button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Remove';
+    deleteButton.addEventListener('click', function () {
+      li.remove();
+    });
+    li.appendChild(deleteButton);//delete button at end of task
   
-  taskList.appendChild(li);
-};
+    taskList.appendChild(li);
+  }
+});
