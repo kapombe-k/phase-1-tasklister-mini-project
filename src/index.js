d56ec3a42894f5aa5fc6a5146aff01c5aec14238
Eugene Kapombe <kapombek@gmail.com>
2025-03-16 16:52:55 +0300

    added event listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // task list starts here:
-  const listedTask = new listedTask();
+  const listedTask = new tasker();
 
   /*
   DOM elements to select:
@@ -11,5 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskDescription = document.getElementById("new-task-description");//description//
   const taskPriority = document.getElementById("new-task-priority");//priority//
 
-  
+  //tasks list on DOM interaction
+  const ulForTasks = document.getElementById("tasks");
+
+  const addTask = () => (ulForTasks.innerHTML = listedTask.renderTasks());
+
+  //addition of event listeners
+
+  newTaskList.addEventListener("submit", (stopper)) = () => {
+    stopper.preventDefault();
+    tasker.creareNewTask(taskDescription.value);
+    
+  }
 });
