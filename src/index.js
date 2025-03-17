8e4d31d18b6a3912f77fc64c39d31c479b717bdb
Eugene Kapombe <kapombek@gmail.com>
2025-03-17 22:14:50 +0300

    start of second attempt

--- src/index.js
+++ src/index.js
@@ -6,48 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
   taskForm.addEventListener('submit', function (event) {   //changed from buttom to form to match first variable
     event.preventDefault();  //prevent reload during submission
-    
-    const addTask = document.querySelector("#new-task-description"); // Corrected selector to use ID for input field
-    
-    //to get input values
-    const taskInput = addTask.value.trim();
-  
-    //to store the todo inside the toDoList array
-    toDoList.push(taskInput.value);
-  
-  
-    //reset form
-    taskForm.reset();
 
-    //iterate through array and outputs
-    renderToDoList(toDoList);
-  });
+    console.log('working');
+  });  
 
-
-  //trying an additional if...else statement to validate text input.
-  //if (taskInput !== '') {
-    //addTaskToList(taskInput);//function to add task
-    //addTask.value = '';
-  //} else {
-    //alert("Enter task here!")//alert when field is empty
-  //
-   
-  const taskList = document.getElementById("tasks");//task container selection
-  
-  // Added function keyword and fixed syntax for task list
-  function addTaskToList(taskInput) {
-    // Create a dynamic list using DOM to add and remove tasks
-    const li = document.createElement("li");
-    li.innerText = taskInput;//list items text
-    
-    //delete task button
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Remove';
-    deleteButton.addEventListener('click', function () {
-      li.remove();
-    });
-    li.appendChild(deleteButton);//delete button at end of task
-    
-    taskList.appendChild(li);
-  }
 });
